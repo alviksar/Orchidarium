15399588dac8181bc100e1964dc9369b2436173b
Alexey Sarafannikov <alviksar@gmail.com>
2018-06-28 18:13:39 +0300

    Add FAB in DetailActivity

--- app/src/main/java/xyz/alviksar/orchidarium/ui/MainActivity.java
+++ app/src/main/java/xyz/alviksar/orchidarium/ui/MainActivity.java
@@ -1,8 +1,9 @@
 package xyz.alviksar.orchidarium.ui;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
 import android.view.View;
 
 import xyz.alviksar.orchidarium.R;
@@ -13,6 +14,44 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mock_activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        /*
+        Set spinner into menu bar
+        Thanks to Dércia Silva
+        http://www.viralandroid.com/2016/03/how-to-add-spinner-dropdown-list-to-android-actionbar-toolbar.html
+        */
+        /*
+        MenuItem item = menu.findItem(R.id.spinner);
+        mSpinner = (Spinner) item.getActionView();
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.spinner_list_item_array, R.layout.spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mSpinner.setAdapter(adapter);
+        // Set spinner to the right state
+        int defPosition = adapter.getPosition(PopularMoviesPreferences.getSort(this));
+        mSpinner.setSelection(defPosition);
+
+        mSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                if (mSpinner.getSelectedItem() != null) {
+                    String sort = (String) mSpinner.getSelectedItem();
+                    PopularMoviesPreferences.setSort(getApplicationContext(), sort);
+                    updateMovies(sort);
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+        */
+        return true;
     }
 
     public void startDetailActivity(View view) {
