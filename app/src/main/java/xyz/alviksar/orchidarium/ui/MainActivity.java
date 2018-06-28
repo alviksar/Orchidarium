e788abea63f536053ab8b3fdb57a42958ab95b47
Alexey Sarafannikov <alviksar@gmail.com>
2018-06-28 19:33:12 +0300

    Add a filter spinner

--- app/src/main/java/xyz/alviksar/orchidarium/ui/MainActivity.java
+++ app/src/main/java/xyz/alviksar/orchidarium/ui/MainActivity.java
@@ -4,11 +4,18 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import xyz.alviksar.orchidarium.R;
+import xyz.alviksar.orchidarium.util.OrchidariumPreferences;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Spinner mSpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,24 +31,25 @@ public class MainActivity extends AppCompatActivity {
         Thanks to Dércia Silva
         http://www.viralandroid.com/2016/03/how-to-add-spinner-dropdown-list-to-android-actionbar-toolbar.html
         */
-        /*
+
         MenuItem item = menu.findItem(R.id.spinner);
         mSpinner = (Spinner) item.getActionView();
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
                 R.array.spinner_list_item_array, R.layout.spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         mSpinner.setAdapter(adapter);
+
         // Set spinner to the right state
-        int defPosition = adapter.getPosition(PopularMoviesPreferences.getSort(this));
+        int defPosition = adapter.getPosition(OrchidariumPreferences.getMode(this));
         mSpinner.setSelection(defPosition);
 
         mSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
                 if (mSpinner.getSelectedItem() != null) {
-                    String sort = (String) mSpinner.getSelectedItem();
-                    PopularMoviesPreferences.setSort(getApplicationContext(), sort);
-                    updateMovies(sort);
+                    String mode = (String) mSpinner.getSelectedItem();
+                    OrchidariumPreferences.setMode(getApplicationContext(), mode);
+                   // TODO: updateList(mode);
                 }
             }
 
@@ -50,7 +58,7 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
-        */
+
         return true;
     }
 
---
+++ app/src/main/java/xyz/alviksar/orchidarium/util/OrchidariumPreferences.java
@@ -0,0 +1,34 @@
+package xyz.alviksar.orchidarium.util;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+import xyz.alviksar.orchidarium.R;
+
+public class OrchidariumPreferences {
+        /**
+         * Helper method to handle setting the mode for the main activity in Preferences
+         *
+         * @param context  Context used to get the SharedPreferences
+         * @param mode     the mode of showing
+         */
+        public static void setMode(Context context, String mode) {
+            SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(context);
+            SharedPreferences.Editor editor = sp.edit();
+            editor.putString(context.getString(R.string.pref_mode_key), mode);
+            editor.apply();
+        }
+
+        /**
+         * Returns the mode currently set in Preferences
+         *
+         * @param context Context used to access SharedPreferences
+         * @return  The mode that current user has set in SharedPreferences or default value.
+         */
+        public static String getMode(Context context) {
+            SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(context);
+            return sp.getString(context.getString(R.string.pref_mode_key),
+                    context.getString(R.string.pref_mode_default));
+        }
+}
