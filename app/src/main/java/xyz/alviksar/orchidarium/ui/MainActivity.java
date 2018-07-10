e55d2b15eccb16ca7cb486b2d0a74e15dbe889c1
Alexey Sarafannikov <alviksar@gmail.com>
2018-07-10 18:02:55 +0300

    Add OrchidViewHolder class

--- app/src/main/java/xyz/alviksar/orchidarium/ui/MainActivity.java
+++ app/src/main/java/xyz/alviksar/orchidarium/ui/MainActivity.java
@@ -5,13 +5,19 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.DisplayMetrics;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -19,14 +25,24 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
 import xyz.alviksar.orchidarium.R;
 import xyz.alviksar.orchidarium.data.OrchidariumPreferences;
 import xyz.alviksar.orchidarium.model.OrchidEntity;
 
+import static android.content.res.Configuration.ORIENTATION_LANDSCAPE;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final Float TILE_WIDTH_INCHES = 1.0f;
 
     @BindView(R.id.sp_filter)
     Spinner mFilterSpinner;
+
+    @BindView(R.id.rv_orchids)
+    RecyclerView mRecyclerView;
+
+    private FirebaseRecyclerAdapter mFirebaseAdapter;
 
     // Firebase
     private FirebaseDatabase mFirebaseDatabase;
@@ -37,7 +53,58 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mock_activity_main_2);
+        ButterKnife.bind(this);
+
+        mDatabaseReference = FirebaseDatabase.getInstance().getReference("orchids");
+//        mRecyclerView = findViewById(R.id.rv_movies);
+//        mLoadingIndicator = findViewById(R.id.pb_loading_indicator);
+//        mErrorMessage = findViewById(R.id.tv_error_message);
+
+        // Calculate the number of columns in the grid
+        DisplayMetrics metrics = getResources().getDisplayMetrics();
+        int mColumnWidthPixels;
+        if (getResources().getConfiguration().orientation == ORIENTATION_LANDSCAPE) {
+            mColumnWidthPixels = Math.round(TILE_WIDTH_INCHES * metrics.ydpi);
+
+        } else {   // ORIENTATION_PORTRAIT
+            mColumnWidthPixels = Math.round(TILE_WIDTH_INCHES * metrics.xdpi);
+
+        }
+        int columns = Math.max(1, metrics.widthPixels / mColumnWidthPixels);
+
+
+        GridLayoutManager layoutManager =
+                new GridLayoutManager(this, columns);
+        // Create PosterAdapter with this context and this OnClickHandler
+
+        mRecyclerView.setLayoutManager(layoutManager);
+        mRecyclerView.setHasFixedSize(true);
+        setUpFirebaseAdapter();
+
     }
+
+    private void setUpFirebaseAdapter() {
+        mFirebaseAdapter = new FirebaseRecyclerAdapter<OrchidEntity, OrchidViewHolder>
+                (OrchidEntity.class, R.layout.orchid_list_item, OrchidViewHolder.class,
+                        mDatabaseReference) {
+
+            @NonNull
+            @Override
+            public OrchidViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                return null;
+            }
+
+            @Override
+            protected void onBindViewHolder(@NonNull OrchidViewHolder holder, int position, @NonNull OrchidEntity model) {
+                holder.bindOrchid(model);
+            }
+
+        };
+        mRecyclerView.setHasFixedSize(true);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mRecyclerView.setAdapter(mFirebaseAdapter);
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -69,10 +136,17 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {
-
+                // Getting Post failed, log a message
+             // TODO:   Log.w(TAG, "loadPost:onCancelled", databaseError.toException());
             }
         };
         mDatabaseReference.addChildEventListener(mChildEventListener);
+
+        /*
+        // TODO:
+        https://www.learnhowtoprogram.com/android/data-persistence/firebase-recycleradapter
+        https://github.com/firebase/FirebaseUI-Android/blob/master/database/README.md
+         */
 
         /*
         Set spinner into menu bar
