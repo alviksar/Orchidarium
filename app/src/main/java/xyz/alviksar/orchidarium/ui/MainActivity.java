3adb0ba1867283b8f15ff976e7fb969c2e6b82d6
Alexey Sarafannikov <alviksar@gmail.com>
2018-07-10 13:51:55 +0300

    Add ChildEventListener in MainActivity

--- app/src/main/java/xyz/alviksar/orchidarium/ui/MainActivity.java
+++ app/src/main/java/xyz/alviksar/orchidarium/ui/MainActivity.java
@@ -2,6 +2,8 @@ package xyz.alviksar.orchidarium.ui;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -10,12 +12,26 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import butterknife.BindView;
 import xyz.alviksar.orchidarium.R;
 import xyz.alviksar.orchidarium.data.OrchidariumPreferences;
+import xyz.alviksar.orchidarium.model.OrchidEntity;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Spinner mSpinner;
+    @BindView(R.id.sp_filter)
+    Spinner mFilterSpinner;
+
+    // Firebase
+    private FirebaseDatabase mFirebaseDatabase;
+    private DatabaseReference mDatabaseReference;
+    private ChildEventListener mChildEventListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,27 +42,58 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        mFirebaseDatabase = FirebaseDatabase.getInstance();
+        mDatabaseReference = mFirebaseDatabase.getReference().child("orchids");
+        mChildEventListener = new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                OrchidEntity orchid = dataSnapshot.getValue(OrchidEntity.class);
+                // TODO: mOrchidAdapter.add(orchid);
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        };
+        mDatabaseReference.addChildEventListener(mChildEventListener);
+
         /*
         Set spinner into menu bar
         Thanks to Dércia Silva
         http://www.viralandroid.com/2016/03/how-to-add-spinner-dropdown-list-to-android-actionbar-toolbar.html
         */
-
-        MenuItem item = menu.findItem(R.id.spinner);
-        mSpinner = (Spinner) item.getActionView();
+        MenuItem item = menu.findItem(R.id.sp_filter);
+        mFilterSpinner = (Spinner) item.getActionView();
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
                 R.array.maint_activity_menu_spinner_items, R.layout.spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        mSpinner.setAdapter(adapter);
+        mFilterSpinner.setAdapter(adapter);
 
         // Set spinner to the right state
         int defPosition = adapter.getPosition(OrchidariumPreferences.getMode(this));
-        mSpinner.setSelection(defPosition);
+        mFilterSpinner.setSelection(defPosition);
 
-        mSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+        mFilterSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int position, long l) {
-                if (mSpinner.getSelectedItem() != null) {
+                if (mFilterSpinner.getSelectedItem() != null) {
                 //    String mode = (String) mSpinner.getSelectedItem();
                //     int position = mSpinner.getSelectedItemPosition();
                  //   OrchidariumPreferences.setMode(getApplicationContext(), mode);
