5519987cea5211e10d964fabfb0d64d7d9d3c766
Alexey Sarafannikov <alviksar@gmail.com>
2018-07-24 13:20:26 +0300

    Add search by a orchid name in MainActivity

--- app/src/main/java/xyz/alviksar/orchidarium/ui/MainActivity.java
+++ app/src/main/java/xyz/alviksar/orchidarium/ui/MainActivity.java
@@ -9,43 +9,38 @@ import android.os.Bundle;
 import android.os.Parcelable;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
 import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.SearchView;
+import android.support.v7.widget.SearchView;
 import android.widget.ProgressBar;
-import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
-
-import java.util.Arrays;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import xyz.alviksar.orchidarium.R;
-import xyz.alviksar.orchidarium.data.OrchidariumPreferences;
 import xyz.alviksar.orchidarium.model.OrchidEntity;
 
 import static android.content.res.Configuration.ORIENTATION_LANDSCAPE;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+        implements SearchView.OnQueryTextListener {
 
     private static final Float TILE_WIDTH_INCHES = 1.0f;
 
@@ -60,44 +55,68 @@ public class MainActivity extends AppCompatActivity {
     @BindView(R.id.tv_error_message)
     TextView mErrorMessage;
 
+    SearchView mSearchView;
+
     // Firebase
     private FirebaseRecyclerAdapter mFirebaseRecyclerAdapter;
-    private Query mQuery;
 
     private Parcelable mSavedRecyclerLayoutState = null;
+    private String mSearchQuery = null;
+    private static final String BUNDLE_SEARCH_QUERY = "MainActivity.mSearchQuery";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        handleIntent(getIntent());
+        ButterKnife.bind(this);
 
         if (savedInstanceState != null) {
             if (savedInstanceState.containsKey(BUNDLE_RECYCLER_LAYOUT))
                 mSavedRecyclerLayoutState = savedInstanceState.getParcelable(BUNDLE_RECYCLER_LAYOUT);
+            if (savedInstanceState.containsKey(BUNDLE_SEARCH_QUERY))
+                mSearchQuery = savedInstanceState.getString(BUNDLE_SEARCH_QUERY);
         }
 
-        ButterKnife.bind(this);
-
-        // Calculate the number of columns in the grid
-        DisplayMetrics metrics = getResources().getDisplayMetrics();
-        int mColumnWidthPixels;
-        if (getResources().getConfiguration().orientation == ORIENTATION_LANDSCAPE) {
-            mColumnWidthPixels = Math.round(TILE_WIDTH_INCHES * metrics.ydpi);
-
-        } else {   // ORIENTATION_PORTRAIT
-            mColumnWidthPixels = Math.round(TILE_WIDTH_INCHES * metrics.xdpi);
-
+        // Check network connection
+        ConnectivityManager cm =
+                (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = cm.getActiveNetworkInfo();
+        // If there is a network connection, fetch data
+        if (networkInfo != null && networkInfo.isConnected()) {
+            // Start watching data
+            setRecyclerAdapter(mSearchQuery);
+            showLoading();
+        } else {
+            // Set no connection error message
+            showErrorMessage(R.string.msg_no_connection_error);
         }
-        int columns = Math.max(1, metrics.widthPixels / mColumnWidthPixels);
+    }
 
+
+    private void setRecyclerAdapter(String searchQuery) {
+        if (mFirebaseRecyclerAdapter != null)
+            mFirebaseRecyclerAdapter.stopListening();
+//        Toast.makeText(this, "Search for'" + searchQuery + "' started.", Toast.LENGTH_LONG).show();
+        Query query;
+        if (TextUtils.isEmpty(searchQuery)) {
+            query = FirebaseDatabase.getInstance()
+                    .getReference()
+                    .child("orchids").orderByChild("saveTime");
+
+        } else {
+            query = FirebaseDatabase.getInstance()
+                    .getReference()
+                    .child("orchids").orderByChild("name")
+                    .startAt(searchQuery)
+                    .endAt(searchQuery + "\uf8ff");
+        }
         /*
         https://github.com/firebase/FirebaseUI-Android/blob/master/database/README.md
          */
         FirebaseRecyclerOptions<OrchidEntity> options =
                 new FirebaseRecyclerOptions.Builder<OrchidEntity>()
-                        .setQuery(mQuery, OrchidEntity.class)
+                        .setQuery(query, OrchidEntity.class)
                         .build();
 
         mFirebaseRecyclerAdapter = new FirebaseRecyclerAdapter<OrchidEntity,
@@ -148,55 +167,32 @@ public class MainActivity extends AppCompatActivity {
 
         };
         mRecyclerView.setAdapter(mFirebaseRecyclerAdapter);
+        // Calculate the number of columns in the grid
+        DisplayMetrics metrics = getResources().getDisplayMetrics();
+        int mColumnWidthPixels;
+        if (getResources().getConfiguration().orientation == ORIENTATION_LANDSCAPE) {
+            mColumnWidthPixels = Math.round(TILE_WIDTH_INCHES * metrics.ydpi);
+
+        } else {   // ORIENTATION_PORTRAIT
+            mColumnWidthPixels = Math.round(TILE_WIDTH_INCHES * metrics.xdpi);
+
+        }
+        int columns = Math.max(1, metrics.widthPixels / mColumnWidthPixels);
+
+
         GridLayoutManager layoutManager =
                 new GridLayoutManager(this, columns);
         mRecyclerView.setLayoutManager(layoutManager);
         mRecyclerView.setHasFixedSize(true);
-
-        // Check network connection
-        ConnectivityManager cm =
-                (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
-        NetworkInfo networkInfo = cm.getActiveNetworkInfo();
-        // If there is a network connection, fetch data
-        if (networkInfo != null && networkInfo.isConnected()) {
-            // Start watching data
+        if (mFirebaseRecyclerAdapter != null)
             mFirebaseRecyclerAdapter.startListening();
-            showLoading();
-        } else {
-            // Set no connection error message
-            showErrorMessage(R.string.msg_no_connection_error);
-        }
     }
-
-    @Override
-    protected void onNewIntent(Intent intent) {
-        setIntent(intent);
-        handleIntent(intent);
-    }
-
-    private void handleIntent(Intent intent) {
-        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
-            String searchQuery = intent.getStringExtra(SearchManager.QUERY);
-            Toast.makeText(this, "Search for'" + searchQuery + "' started.", Toast.LENGTH_LONG).show();
-            mQuery = FirebaseDatabase.getInstance()
-                    .getReference()
-                    .child("orchids").orderByChild("name")
-                    .startAt(searchQuery)
-                    .endAt(searchQuery + "\uf8ff");
-        } else {
-            mQuery = FirebaseDatabase.getInstance()
-                    .getReference()
-                    .child("orchids").orderByChild("saveTime");
-        }
-    }
-
 
     @Override
     protected void onDestroy() {
         mFirebaseRecyclerAdapter.stopListening();
         super.onDestroy();
     }
-
 
     /**
      * https://stackoverflow.com/questions/27816217/how-to-save-recyclerviews-scroll-position-using-recyclerview-state
@@ -207,14 +203,16 @@ public class MainActivity extends AppCompatActivity {
         if (savedInstanceState != null) {
             if (savedInstanceState.containsKey(BUNDLE_RECYCLER_LAYOUT))
                 mSavedRecyclerLayoutState = savedInstanceState.getParcelable(BUNDLE_RECYCLER_LAYOUT);
+            if (savedInstanceState.containsKey(BUNDLE_SEARCH_QUERY))
+                mSearchQuery = savedInstanceState.getString(BUNDLE_SEARCH_QUERY);
         }
     }
-
 
     @Override
     public void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
         outState.putParcelable(BUNDLE_RECYCLER_LAYOUT, mRecyclerView.getLayoutManager().onSaveInstanceState());
+        outState.putString(BUNDLE_SEARCH_QUERY, mSearchQuery);
     }
 
     @Override
@@ -223,11 +221,32 @@ public class MainActivity extends AppCompatActivity {
 
         // Get the SearchView and set the searchable configuration
         SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
-        SearchView searchView = (SearchView) menu.findItem(R.id.action_filter).getActionView();
+        mSearchView = (SearchView) menu.findItem(R.id.action_search).getActionView();
         // Assumes current activity is the searchable activity
-        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
-        searchView.setIconifiedByDefault(true);
-        searchView.setSubmitButtonEnabled(true);
+        mSearchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+        mSearchView.setOnQueryTextListener(this);
+
+        mSearchView.setIconifiedByDefault(true);
+        mSearchView.setSubmitButtonEnabled(true);
+        mSearchView.setQuery(mSearchQuery, false);
+        mSearchView.clearFocus();
+
+        mSearchView.setOnSearchClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mSearchView.setQuery(mSearchQuery, false);
+                mSearchView.clearFocus();
+            }
+        });
+        mSearchView.setOnCloseListener(new SearchView.OnCloseListener() {
+            @Override
+            public boolean onClose() {
+                mSearchQuery = "";
+                setRecyclerAdapter(mSearchQuery);
+                return false;
+            }
+        });
+
 
         return true;
     }
@@ -280,5 +299,17 @@ public class MainActivity extends AppCompatActivity {
         mLoadingIndicator.setVisibility(View.GONE);
         mErrorMessage.setVisibility(View.GONE);
         mRecyclerView.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    public boolean onQueryTextSubmit(String searchQuery) {
+        setRecyclerAdapter(searchQuery);
+        return false;
+    }
+
+    @Override
+    public boolean onQueryTextChange(String newText) {
+        mSearchQuery = newText;
+        return false;
     }
 }
